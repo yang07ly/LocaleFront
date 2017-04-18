e5f706c8daee59750b7ee5662ccd34829b467b1e
Yang <yang@leandatainc.com>
2017-04-18 00:46:45 -0700

    change to Locale customized ui outlook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: '../templates/index.html'
+  templateUrl: '../index.html'
 })
 export class AppComponent {
   title = 'Welcome to Locale!\r\n Frontend Development work still in progress. Please stay tune!';
