260dd3ce822e5788bb25a0b3ac71032c6430ef82
Yang <yang@leandatainc.com>
2017-04-25 01:27:04 -0700

    Fix Heroku Deployment Problem. Don't complain about Commit message practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 declare var jQuery:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'Welcome to Locale!\r\n Frontend Development work still in progress. Please stay tune!';
-
-  ngAfterViewInit() {
-  }
 }
