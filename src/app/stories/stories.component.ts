8b29d9f6891e53c83bc2f3b7d6e7f0536183f69a
Yang <yang@leandatainc.com>
2017-04-25 18:49:42 -0700

    resolve jquery dependencies error

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -8,26 +8,25 @@ declare var jQuery:any;
 
 export class StoriesComponent implements AfterViewInit {
 	constructor() {
+		
 	}
 
 	ngAfterViewInit() {
-		jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js', function(){});
-		jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js', function(){});
-		jQuery.getScript('assets/js/bootstrap-select.min.js', function(){});
-		jQuery.getScript('assets/js/jquery.countdown.min.js', function(){});
-		jQuery.getScript('assets/js/bootstrap.min.js', function(){});
-		jQuery.getScript('assets/js/countdown.js', function(){});
-		jQuery.getScript('assets/js/jquery-3.1.0.min.js', function(){});
-		jQuery.getScript('assets/js/richmarker.js', function(){
-			jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){
-				jQuery.getScript('assets/js/sweetalert.min.js', function(){
-					jQuery.getScript('assets/js/owl.carousel.min.js', function(){
-						jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){
-							jQuery.getScript('assets/js/scripts.js', function(){});
-						});
-					});
-				});
+		jQuery.when(
+			jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js'),
+			jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js'),
+			jQuery.getScript('assets/js/bootstrap-select.min.js'),
+			jQuery.getScript('assets/js/jquery.countdown.min.js'),
+			jQuery.getScript('assets/js/bootstrap.min.js'),
+			jQuery.getScript('assets/js/countdown.js'),
+			jQuery.getScript('assets/js/jquery-3.1.0.min.js'),
+			jQuery.getScript('assets/js/jquery.nicescroll.min.js'),
+			jQuery.getScript('assets/js/jquery.nouislider.all.min.js'),
+			jQuery.getScript('assets/js/owl.carousel.min.js'),
+			jQuery.getScript('assets/js/richmarker.js'),
+			jQuery.getScript('assets/js/sweetalert.min.js')
+			).done(function() {
+				jQuery.getScript('assets/js/scripts.js');
 			});
-		});
 	}
 }
