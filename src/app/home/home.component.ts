3d0acfd5f415de49cb08446d8e7c10da4bd9ea1b
Yang <yang@leandatainc.com>
2017-04-25 18:25:54 -0700

    resolve jquery dependencies error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,12 @@ declare var jQuery:any;
 export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
     jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js', function(){});
     jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js', function(){});
     jQuery.getScript('assets/js/bootstrap-select.min.js', function(){});
@@ -16,17 +22,39 @@ export class HomeComponent implements OnInit, AfterViewInit {
     jQuery.getScript('assets/js/bootstrap.min.js', function(){});
     jQuery.getScript('assets/js/countdown.js', function(){});
     jQuery.getScript('assets/js/jquery-3.1.0.min.js', function(){});
-    jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){});
-    jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){});
-    jQuery.getScript('assets/js/owl.carousel.min.js', function(){});
-    jQuery.getScript('assets/js/richmarker.js', function(){});
-    jQuery.getScript('assets/js/sweetalert.min.js', function(){});
-  }
+    jQuery.getScript('assets/js/richmarker.js', function(){
+      jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){
+        jQuery.getScript('assets/js/sweetalert.min.js', function(){
+          jQuery.getScript('assets/js/owl.carousel.min.js', function(){
+            jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){
+              jQuery.getScript('assets/js/scripts.js', function(){});
+            });
+          });
+        });
+      });
+    });
 
-  ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-  	jQuery.getScript('assets/js/scripts.js', function(){});
+    
+    /*jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js', function(){});
+    jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js', function(){});
+    jQuery.getScript('assets/js/bootstrap-select.min.js', function(){});
+    jQuery.getScript('assets/js/jquery.countdown.min.js', function(){});
+    jQuery.getScript('assets/js/bootstrap.min.js', function(){
+      jQuery.getScript('assets/js/countdown.js', function(){
+        jQuery.getScript('assets/js/jquery-3.1.0.min.js', function(){
+          jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){
+            jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){
+              jQuery.getScript('assets/js/owl.carousel.min.js', function(){
+                jQuery.getScript('assets/js/richmarker.js', function(){
+                  jQuery.getScript('assets/js/sweetalert.min.js', function(){
+                    jQuery.getScript('assets/js/scripts.js', function(){});
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });*/
   }
 }
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -8,6 +8,9 @@ declare var jQuery:any;
 
 export class StoriesComponent implements AfterViewInit {
 	constructor() {
+	}
+
+	ngAfterViewInit() {
 		jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js', function(){});
 		jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js', function(){});
 		jQuery.getScript('assets/js/bootstrap-select.min.js', function(){});
@@ -15,14 +18,16 @@ export class StoriesComponent implements AfterViewInit {
 		jQuery.getScript('assets/js/bootstrap.min.js', function(){});
 		jQuery.getScript('assets/js/countdown.js', function(){});
 		jQuery.getScript('assets/js/jquery-3.1.0.min.js', function(){});
-		jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){});
-		jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){});
-		jQuery.getScript('assets/js/owl.carousel.min.js', function(){});
-		jQuery.getScript('assets/js/richmarker.js', function(){});
-		jQuery.getScript('assets/js/sweetalert.min.js', function(){});
-	}
-	
-	ngAfterViewInit() {
-		jQuery.getScript('assets/js/scripts.js', function(){});
+		jQuery.getScript('assets/js/richmarker.js', function(){
+			jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){
+				jQuery.getScript('assets/js/sweetalert.min.js', function(){
+					jQuery.getScript('assets/js/owl.carousel.min.js', function(){
+						jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){
+							jQuery.getScript('assets/js/scripts.js', function(){});
+						});
+					});
+				});
+			});
+		});
 	}
 }
