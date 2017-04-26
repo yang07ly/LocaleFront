537ff5adde1e394f8783ace97cf649e79508f5a9
Yang <yang@leandatainc.com>
2017-04-25 19:09:53 -0700

    solve js dependency issue

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -12,21 +12,21 @@ export class StoriesComponent implements AfterViewInit {
 	}
 
 	ngAfterViewInit() {
-		jQuery.when(
-			jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js'),
-			jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js'),
-			jQuery.getScript('assets/js/bootstrap-select.min.js'),
-			jQuery.getScript('assets/js/jquery.countdown.min.js'),
-			jQuery.getScript('assets/js/bootstrap.min.js'),
-			jQuery.getScript('assets/js/countdown.js'),
-			jQuery.getScript('assets/js/jquery-3.1.0.min.js'),
-			jQuery.getScript('assets/js/jquery.nicescroll.min.js'),
-			jQuery.getScript('assets/js/jquery.nouislider.all.min.js'),
-			jQuery.getScript('assets/js/owl.carousel.min.js'),
-			jQuery.getScript('assets/js/richmarker.js'),
-			jQuery.getScript('assets/js/sweetalert.min.js')
-			).done(function() {
-				jQuery.getScript('assets/js/scripts.js');
-			});
+		// jQuery.when(
+		// 	jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js'),
+		// 	jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js'),
+		// 	jQuery.getScript('assets/js/bootstrap-select.min.js'),
+		// 	jQuery.getScript('assets/js/jquery.countdown.min.js'),
+		// 	jQuery.getScript('assets/js/bootstrap.min.js'),
+		// 	jQuery.getScript('assets/js/countdown.js'),
+		// 	jQuery.getScript('assets/js/jquery-3.1.0.min.js'),
+		// 	jQuery.getScript('assets/js/jquery.nicescroll.min.js'),
+		// 	jQuery.getScript('assets/js/jquery.nouislider.all.min.js'),
+		// 	jQuery.getScript('assets/js/owl.carousel.min.js'),
+		// 	jQuery.getScript('assets/js/richmarker.js'),
+		// 	jQuery.getScript('assets/js/sweetalert.min.js')
+		// 	).done(function() {
+		// 		jQuery.getScript('assets/js/scripts.js');
+		// 	});
 	}
 }
