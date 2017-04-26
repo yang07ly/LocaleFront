37da2ee88d7c1202b2847992645463cba9c58656
Yang <yang@leandatainc.com>
2017-04-25 21:12:44 -0700

    removed commented wrapped part in scripts.js

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,42 +16,5 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
 	ngAfterViewInit() {
 		jQuery.getScript('assets/js/scripts.js');
-		// jQuery.when(
-		// 	jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js'),
-		// 	jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js'),
-		// 	jQuery.getScript('assets/js/bootstrap-select.min.js'),
-		// 	jQuery.getScript('assets/js/jquery.countdown.min.js'),
-		// 	jQuery.getScript('assets/js/bootstrap.min.js'),
-		// 	jQuery.getScript('assets/js/countdown.js'),
-		// 	jQuery.getScript('assets/js/jquery-3.1.0.min.js'),
-		// 	jQuery.getScript('assets/js/jquery.nicescroll.min.js'),
-		// 	jQuery.getScript('assets/js/jquery.nouislider.all.min.js'),
-		// 	jQuery.getScript('assets/js/owl.carousel.min.js'),
-		// 	jQuery.getScript('assets/js/richmarker.js'),
-		// 	jQuery.getScript('assets/js/sweetalert.min.js')
-		// 	).done(function() {
-		// 		jQuery.getScript('assets/js/scripts.js');
-		// 	});
-		/*jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.js', function(){});
-		jQuery.getScript('assets/scripts/Nivo-Lightbox/nivo-lightbox.min.js', function(){});
-		jQuery.getScript('assets/js/bootstrap-select.min.js', function(){});
-		jQuery.getScript('assets/js/jquery.countdown.min.js', function(){});
-		jQuery.getScript('assets/js/bootstrap.min.js', function(){
-			jQuery.getScript('assets/js/countdown.js', function(){
-				jQuery.getScript('assets/js/jquery-3.1.0.min.js', function(){
-					jQuery.getScript('assets/js/jquery.nicescroll.min.js', function(){
-						jQuery.getScript('assets/js/jquery.nouislider.all.min.js', function(){
-							jQuery.getScript('assets/js/owl.carousel.min.js', function(){
-								jQuery.getScript('assets/js/richmarker.js', function(){
-									jQuery.getScript('assets/js/sweetalert.min.js', function(){
-										jQuery.getScript('assets/js/scripts.js', function(){});
-									});
-								});
-							});
-						});
-					});
-				});
-			});
-		});*/
 	}
 }
