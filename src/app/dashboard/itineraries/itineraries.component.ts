ebcfcdf3b8315048770b1008f06a4fe728dd9891
Yang <yang@leandatainc.com>
2017-05-02 00:52:53 -0700

    fixed map rendering problem

--- src/app/dashboard/itineraries/itineraries.component.ts
+++ src/app/dashboard/itineraries/itineraries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 declare var $:any;
+declare function initialize_map();
 
 @Component({
   selector: 'app-itineraries',
@@ -15,5 +16,6 @@ export class ItinerariesComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     $.getScript('assets/js/scripts.js');
+    initialize_map();
   }
 }
